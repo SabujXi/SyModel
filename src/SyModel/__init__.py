1478dfd4ecd8bf4400f7c1c1352a70bfaae2ab06
sabuj <md.sabuj.sarker@gmail.com>
2019-11-01 23:32:46 +0600

    Moved model & model parser from synamic framework; started in 2017

---
+++ src/SyModel/__init__.py
@@ -0,0 +1,1 @@
+from .model_parser import Model, ModelField, ModelParser
---
+++ src/SyModel/exceptions.py
@@ -0,0 +1,31 @@
+class SyModelException(Exception):
+    pass
+
+
+class SynamicModelParsingError(SyModelException):
+    """When model cannot be parsed properly"""
+
+
+def get_source_snippet_from_text(text, line_no, limit=10):
+    """Line no starts at 1 not at 0"""
+    source = text
+    lines = source.splitlines()
+
+    half_limit = limit//2
+    half_limit_l = half_limit
+    if line_no > half_limit:
+        half_limit_l = line_no - half_limit
+    else:
+        half_limit_l = 1
+    res = []
+    res.extend(
+        ['.... ' + line for line in lines[half_limit_l - 1:line_no - 1]]
+    )
+
+    error_line = lines[line_no - 1]
+    res.append('->.. ' + error_line)
+
+    res.extend(
+        ['.... ' + line for line in lines[line_no:line_no + half_limit]]
+    )
+    return '\n'.join(res)
